be463d06fba70c418971f4bbcf1739c697ad21d3
adambigg-s <biggsa601@gmail.com>
2025-03-18 02:02:44 -0400

    update

--- src/main.c
+++ src/main.c
@@ -6,18 +6,20 @@
 int main(int argc, char *argv[]) {
     Camera camera = camera_build(400, 400 * 9 / 16, 50);
 
-    Material mat_ground = {.type = Lambertian, .albedo = vec3_build(0.8, 0.8, 0.0)};
+    Material mat_ground = {.type = Lambertian, .albedo = vec3_build(0.2, 0.2, 0.2)};
     Material mat_center = {.type = Lambertian, .albedo = vec3_build(0.1, 0.2, 0.5)};
+    Material mat_back = {.type = Metal, .albedo = vec3_build(0.3, 0.8, 0.3), .fuzzy = 0.1};
     Material mat_left = {.type = Metal, .albedo = vec3_build(0.8, 0.8, 0.8), .fuzzy = 0.0};
-    Material mat_right = {.type = Metal, .albedo = vec3_build(0.8, 0.6, 0.2), .fuzzy = 0.75};
+    Material mat_right = {.type = Metal, .albedo = vec3_build(0.8, 0.6, 0.2), .fuzzy = 0.5};
     Material mat_little = {.type = Metal, .albedo = vec3_build(0, 1, 1), .fuzzy = 0.2};
 
     SphereList scene = spherelist_new();
-    spherelist_add(&scene, sphere_build(vec3_build(0, -100.5, -1), 100, &mat_ground));
-    spherelist_add(&scene, sphere_build(vec3_build(0, 0, -1.3), 0.5, &mat_center));
-    spherelist_add(&scene, sphere_build(vec3_build(-1.0, -0.2, -0.9), 0.3, &mat_left));
+    spherelist_add(&scene, sphere_build(vec3_build(0, -400.5, -1), 400, &mat_ground));
+    spherelist_add(&scene, sphere_build(vec3_build(0.1, 0, -1.5), 0.5, &mat_left));
+    spherelist_add(&scene, sphere_build(vec3_build(-1.0, -0.2, -0.9), 0.3, &mat_center));
     spherelist_add(&scene, sphere_build(vec3_build(1.1, 0, -1.1), 0.5, &mat_right));
     spherelist_add(&scene, sphere_build(vec3_build(0.5, -0.4, -0.8), 0.1, &mat_little));
+    spherelist_add(&scene, sphere_build(vec3_build(-1.5, 0, -3), 0.5, &mat_back));
 
     char *output_file = (argc > 1) ? argv[1] : "./images/dump.ppm";
     FILE *file;
